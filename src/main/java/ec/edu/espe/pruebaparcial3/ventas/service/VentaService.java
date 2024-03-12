c5f815101b6731604a944f97253485d6cf7c3b6e
Andres Rodriguez <carodriguez19@espe.edu.ec>
2024-03-11 20:50:36 -0500

    8:50

--- src/main/java/ec/edu/espe/pruebaparcial3/ventas/service/VentaService.java
+++ src/main/java/ec/edu/espe/pruebaparcial3/ventas/service/VentaService.java
@@ -1,4 +1,14 @@
 package ec.edu.espe.pruebaparcial3.ventas.service;
 
+import ec.edu.espe.pruebaparcial3.ventas.repository.VentaRepository;
+import org.springframework.stereotype.Service;
+
+@Service
 public class VentaService {
+    private final VentaRepository ventaRepository;
+
+    public VentaService(VentaRepository ventaRepository) {
+        this.ventaRepository = ventaRepository;
+    }
+
 }
